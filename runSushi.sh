1bdb44e6e51608e166f348654f8bdd50baaecaf1
John Carter <john.carter@health.govt.nz>
2023-02-03 15:03:39 +1300

    commented out local scripts

--- runSushi.sh
+++ runSushi.sh
@@ -6,15 +6,15 @@ then
 
 # create the 2 summary files extensions.xml & profiles.xml. Saves in both /input.. & /fsh/ig-data...
 
-echo "Creating Profiles and extensions summary pages..."
-./localscripts/makeProfilesAndExtensions.js nhi
+# echo "Creating Profiles and extensions summary pages..."
+# ./localscripts/makeProfilesAndExtensions.js nhi
 
 
-echo "Making terminology summary"   # will copy into IG input folder
-./localscripts/makeTerminologySummary.js nhi
+# echo "Making terminology summary"   # will copy into IG input folder
+# ./localscripts/makeTerminologySummary.js nhi
 
-echo "Making API summary"
-./localscripts/makeCapabilityStatement.js .
+# echo "Making API summary"
+# ./localscripts/makeCapabilityStatement.js .
 
 #echo "Making NamingSystem summary"
 #../scripts/makeNamingSystemSummary.js nhi

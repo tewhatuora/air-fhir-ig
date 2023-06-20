51ea89ffc5e52ec11d6fb7be07dd42a6a5b42b27
AirMaintainer <air.maintainer@tewhatuora.govt.nz>
2023-06-20 12:40:38 +1200

    Remove unnecassary logging

--- runSushi.sh
+++ runSushi.sh
@@ -1,28 +1,3 @@
 
 
-sushi  -s -o .
-if [[ $? -eq 0 ]]
-then
-
-echo "SUSHI successful! That's eely good news!"
-
-# create the 2 summary files extensions.xml & profiles.xml. Saves in both /input.. & /fsh/ig-data...
-
-# echo "Creating Profiles and extensions summary pages..."
-# ./localscripts/makeProfilesAndExtensions.js nhi
-
-
-# echo "Making terminology summary"   # will copy into IG input folder
-# ./localscripts/makeTerminologySummary.js nhi
-
-# echo "Making API summary"
-# ./localscripts/makeCapabilityStatement.js .
-
-#echo "Making NamingSystem summary"
-#../scripts/makeNamingSystemSummary.js nhi
-
-else 
-echo
-echo
-echo "There were errors, so the other scripts weren't run..."
-fi
+sushi -s -o .

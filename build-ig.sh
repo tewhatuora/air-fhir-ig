72b800c8a418f9c129379295114b7e0fc681df7e
Hayden Bakkum <hayden.bakkum@health.govt.nz>
2023-07-05 11:41:19 +1200

    test remove proxy fixes cicd

--- build-ig.sh
+++ build-ig.sh
@@ -7,5 +7,4 @@ echo running sushi ...
 
 echo running ig publisher
 java -Xms2g -Xmx2g -jar input-cache/publisher.jar -ig . \
-    -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 \
     -no-sushi

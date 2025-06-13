6db55c18452c610a18e7170f359331556754479e
Laura Phillipson <laura.phillipson@tewhatuora.govt.nz>
2025-06-13 14:40:04 +1200

    parameter added to build

--- build-ig.sh
+++ build-ig.sh
@@ -53,7 +53,7 @@ if [[ "$sushi_only" != "true" ]]; then
     fi
 
     echo running ig publisher
-    java $JAVA_OPTS -jar input-cache/publisher.jar -ig . $IG_OPTS -no-sushi
+    java $JAVA_OPTS -jar input-cache/publisher.jar -ig . $IG_OPTS -no-sushi -authorise-non-conformant-tx-servers
 fi
 
 echo running PlantUML ...

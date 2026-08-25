d40175d08182ab637180c1424efa35b00daa6b0a
bsmith <brook.smith@tewhatuora.govt.nz>
2026-08-25 14:28:49 +1200

    update git ignore

--- .builder/scripts/post_updateurls.sh
+++ .builder/scripts/post_updateurls.sh
@@ -3,11 +3,8 @@ set -eo pipefail
 
 # $ENV should be set by the builder
 
-if [[ "${ENV}" != "uat"  ]]; then
-  echo "Only works on the uat branch, found ${CI_COMMIT_BRANCH}"
-  exit
+if [[ "${ENV}" == "uat"  ]]; then
+  echo "Changing domain in html files from \"fhir-ig.digital.health.nz\" to \"fhir-ig-uat.digital.health.nz\""
+  # update the urls, so navigation remains in the uat preview
+  (cd output; sed -i 's|fhir-ig.digital.health.nz|fhir-ig-uat.digital.health.nz|g' *.html)
 fi
-
-echo "Changing domain in html files from \"fhir-ig.digital.health.nz\" to \"fhir-ig-uat.digital.health.nz\""
-# update the urls, so navigation remains in the uat preview
-(cd output; sed -i 's|fhir-ig.digital.health.nz|fhir-ig-uat.digital.health.nz|g' *.html)
